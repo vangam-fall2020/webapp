bf2e852726b149703d3bd179064c4289b95028ff
Manasa Vanga <vanga.m@northeastern.edu>
2020-11-09 14:58:18 -0700

    A7 (#43)
    * logging and statsD changes
    
    * cloudwatch changes
    
    * cloudwatch changes 1

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -3,7 +3,7 @@ pwd
 whoami
 aws configure set default.region us-east-1
 aws configure list
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-agent-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/amazon-cloudwatch-agent.json -s
 cd ~
 sudo chown -R ubuntu:ubuntu webapp
 cd webapp
